a2d96252724e5971a74d9f5d742d8a30c3dd0885
Filzi Jelila <Filzi Jelila@FILZI>
2026-04-18 03:57:23 +0700

    Tugas Laprak2

---
+++ PrakSD2026_A_2511533019/src/pekan2_2511533019/Musik_2511533019.java
@@ -0,0 +1,36 @@
+package pekan2_2511533019;
+
+public class Musik_2511533019 {
+	private String Judul_3019;
+	private String Penyanyi_3019;
+	private int durasi_3019;
+
+	//konstruktor
+	public Musik_2511533019(String judul, String penyanyi, int durasi) {
+		this.Judul_3019 = judul;
+		this.Penyanyi_3019 = penyanyi;
+		this.durasi_3019 = durasi;
+	}
+
+	//getter
+	public String getJudul_3019() {
+	    return Judul_3019;
+	}
+	public String getPenyanyi_3019() {
+	    return Penyanyi_3019;
+	}
+	public int getDurasi_3019() {
+	    return durasi_3019;
+	}
+	
+	//setter
+	public void setJudul_3019(String judul) {
+	     this.Judul_3019 = judul;
+	}
+	public void setPenyanyi_3019(String penyanyi) {
+	     this.Penyanyi_3019 = penyanyi;
+	}
+	public void setDurasi_3019(int durasi) {
+	     this.durasi_3019 = durasi;
+	}
+}
